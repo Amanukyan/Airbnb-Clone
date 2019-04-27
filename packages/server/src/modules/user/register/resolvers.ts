a6ba957b3b329a7e935a608696612df68c52e640
alex <amanukya@student.42.fr>
2019-04-26 18:36:19 -0700

    Calling the Register Mutation

--- packages/server/src/modules/user/register/resolvers.ts
+++ packages/server/src/modules/user/register/resolvers.ts
@@ -4,16 +4,12 @@ import { ResolverMap } from "../../../types/graphql-utils";
 import { User } from "../../../entity/User";
 import { formatYupError } from "../../../utils/formatYupError";
 import { duplicateEmail } from "./errorMessages";
-// import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
-// import { sendEmail } from "../../utils/sendEmail";
+import { createConfirmEmailLink } from "./createConfirmEmailLink";
+import { sendEmail } from "../../../utils/sendEmail";
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    register: async (
-      _,
-      args,
-      // { redis, url }
-    ) => {
+    register: async (_, args, { redis, url }) => {
       try {
         await validUserSchema.validate(args, { abortEarly: false });
       } catch (err) {
@@ -43,12 +39,12 @@ export const resolvers: ResolverMap = {
 
       await user.save();
 
-      // if (process.env.NODE_ENV !== "test") {
-      //   await sendEmail(
-      //     email,
-      //     await createConfirmEmailLink(url, user.id, redis)
-      //   );
-      // }
+      if (process.env.NODE_ENV !== "test") {
+        await sendEmail(
+          email,
+          await createConfirmEmailLink(url, user.id, redis)
+        );
+      }
 
       return null;
     }

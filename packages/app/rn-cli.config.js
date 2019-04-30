cda6372c53706df52c12e0c7bdb3bb132103e6d8
alex <amanukya@student.42.fr>
2019-04-30 10:17:18 -0700

    Styling React Native Register Form

--- packages/app/rn-cli.config.js
+++ packages/app/rn-cli.config.js
@@ -2,12 +2,13 @@ const blacklist = require("metro-config/src/defaults/blacklist");
 const path = require("path");
 
 module.exports = {
-  extraNodeModules: {
-    "react-native": path.resolve(__dirname, "node_modules/react-native")
-  },
   getBlacklistRE() {
     return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
   },
   projectRoot: path.resolve(__dirname),
-  watchFolders: [path.resolve(__dirname, "../..", "node_modules")]
+  watchFolders: [
+    path.resolve(__dirname, "../..", "node_modules"),
+    path.resolve(__dirname, "../common"),
+    path.resolve(__dirname, "../controller")
+  ]
 };

a1a141dbfedd43c62775974dc49689465e75b481
alex <amanukya@student.42.fr>
2019-05-09 08:21:12 -0700

    Create Listing GraphQL Higher Order Component

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -1,12 +1,9 @@
 export * from "./modules/RegisterController";
-
 export * from "./modules/LoginController";
-
 export * from "./modules/ForgotPasswordController";
-
 export * from "./modules/ChangePasswordController";
 export * from "./modules/ChangePasswordController/__generated__/ForgotPasswordChangeMutation";
-
+export * from "./modules/CreateListing";
 export * from "./modules/auth/AuthRoute";
 
 export * from "./types/NormalizedErrorMap";

db5aa75546096816315df6f7b810e3eb9792b2c9
alex <amanukya@student.42.fr>
2019-05-12 15:42:02 -0700

    GraphQL Subscriptions with GraphQL Yoga

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import { applyMiddleware } from "graphql-middleware";
 import * as express from "express";
 var session = require("express-session");
@@ -27,15 +27,18 @@ export const startServer = async () => {
   const schema = genSchema() as any;
   applyMiddleware(schema, middleware);
 
+  const pubsub = new PubSub();
+
   const server = new GraphQLServer({
     schema,
     context: ({ request, response }) => ({
       redis,
-      url: request.protocol + "://" + request.get("host"),
-      session: request.session,
+      url: request ? request.protocol + "://" + request.get("host") : "",
+      session: request ? request.session : undefined,
       req: request,
       res: response,
-      userLoader: userLoader()
+      userLoader: userLoader(),
+      pubsub
     })
   });
 

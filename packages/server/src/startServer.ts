3c69abbe18858282103319e539eec97592b6f29f
alex <amanukya@student.42.fr>
2019-05-13 09:45:55 -0700

    Cache GraphQL Resolver with Redis

--- packages/server/src/startServer.ts
+++ packages/server/src/startServer.ts
@@ -12,10 +12,11 @@ import { redis } from "./redis";
 import { createTypeormConn } from "./utils/createTypeormConn";
 import { confirmEmail } from "./routes/confirmEmail";
 import { genSchema } from "./utils/genSchema";
-import { redisSessionPrefix } from "./constants";
+import { redisSessionPrefix, listingCacheKey } from "./constants";
 import { createTestConn } from "./testUtils/createTestConn";
 import { middleware } from "./middleware";
 import { userLoader } from "./loaders/UserLoader";
+import { Listing } from "./entity/Listing";
 
 const SESSION_SECRET = "qfzf32ff2Ò";
 var RedisStore = require("connect-redis")(session);
@@ -96,6 +97,14 @@ export const startServer = async () => {
     const conn = await createTypeormConn();
     await conn.runMigrations();
   }
+
+  // clear cache
+  await redis.del(listingCacheKey);
+  // fill cache
+  const listings = await Listing.find();
+  const listingStrings = listings.map(x => JSON.stringify(x));
+  await redis.lpush(listingCacheKey, ...listingStrings);
+  // console.log(await redis.lrange(listingCacheKey, 0, -1));
 
   const port = process.env.PORT || 4000;
   const app = await server.start({
